170402147482c4d0f536e61c6eb80102d4be3a62
Aryan Sharma <90350395+realaryansharma@users.noreply.github.com>
2023-10-30 12:24:46 +0530

    welcome back

---
+++ Recursion/Java/Problems.md
@@ -0,0 +1,8 @@
+3 steps of Recusrion:
+- Finding the base case
+- Finding the relation between problems and sub-problems
+- Generalizing the realtion
+
++++++++++++++++++++++++++++++++++++
+
+1. Number of ways to reach a particular block in a n X m matrix (MAZE)
---
+++ sorting/SortArr.java
@@ -0,0 +1,30 @@
+class SortArr {
+
+    private int[] partition() {
+        
+        int pivot = arr[0], i = 0, j = arr.length;
+
+        while() {
+
+        }
+
+    }
+    
+    private int[] quickSort(int[] arr) {
+
+        
+
+        return arr;
+    }
+
+    public static void main(String args[]) {
+
+        int[] arr = {2, 3, 1, 7, 6, 9};        
+
+        SortArr jarvis = new SortArr();
+
+        arr = jarvis.quickSort(arr);
+
+    }
+
+}
